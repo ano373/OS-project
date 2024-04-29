9474bdcf43880ce1a5cdb8309039046e1f1e3b12
ano373 <96596021+ano373@users.noreply.github.com>
2024-04-29 22:54:38 +0300

    Update LRU.cpp
    made the code more clean, made a function for LRU

--- LRU.cpp
+++ LRU.cpp
@@ -4,13 +4,13 @@ using namespace std;
 
 
 int duplicate(vector<int> list, int start, int end){
-    vector<int> sortlist(list.begin() + start, list.begin() + end + 1);
+    vector<int> newlist(list.begin() + start, list.begin() + end + 1);
 
-    sort(sortlist.begin(),sortlist.end());
+    sort(newlist.begin(),newlist.end());
     int count =0;
     
-    for(int i=0;i<sortlist.size()-1;i++){
-        if(sortlist[i]==sortlist[i+1]){
+    for(int i=0;i<newlist.size()-1;i++){
+        if(newlist[i]==newlist[i+1]){
             count++;
         }
     }
@@ -44,37 +44,35 @@ void printAns(int pageSize,vector<vector<int>> ans,vector<int> list){
 
 }
 
+vector<int> LRU(vector<int> curr,vector<int> list,int pos,int pageSize){
+    
+    int count =0;
+    int end = pos-1;
+    int start = end-pageSize +1;
+    while(duplicate(list,start,end)-count!=0){
+        start -= duplicate(list,start,end);
+        count += duplicate(list,start,end);            
+    } 
+    
+    int index = findIndex(curr,pageSize,list[start]);
+    curr[index] = list[pos];
+    return curr;
+}
+
 int main() {
-    vector<int> list = {7,0,1,2,0,3,7,0,3,2,3,7,0,5,8,9,0,0,4,2,3,0,3,2,1,2,0,1,7,0,1};
+    vector<int> list = {7,0,1,2,0,3,0,4,2,3,0,3,2,1,2,0,1,7,0,1};
     vector<vector<int>> ans;
-    int pageSize = 4;
+    int pageSize = 3;
     vector<int> curr;
 
     for(int i=0;i<list.size();i++){
         if(i<pageSize)
             curr.push_back(list[i]);
-        
-        
-        
         //replacement LRU
-        else if(findIndex(curr,pageSize,list[i])==-1){
-            int count =0;
-            int end = i-1;
-            int start = end-pageSize +1;
-            while(duplicate(list,start,end)-count!=0)
-            {
-                start -= duplicate(list,start,end);
-                count += duplicate(list,start,end);            
-            } 
-            
-            int index = findIndex(curr,pageSize,list[start]);
-            curr[index] = list[i];
-         }
-         
+        else if(findIndex(curr,pageSize,list[i])==-1)
+            curr = LRU(curr,list,i,pageSize);
         ans.push_back(curr);
     }
-
-   printAns(pageSize,ans,list);     
-    
+    printAns(pageSize,ans,list);     
     return 0;
 }
