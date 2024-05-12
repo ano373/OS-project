62f85d2aa7676f227711c518db1d0ace728cc188
ano373 <96596021+ano373@users.noreply.github.com>
2024-05-12 22:35:04 +0300

    Update fifo.cpp "changed curr to frame"

--- fifo.cpp
+++ fifo.cpp
@@ -28,34 +28,34 @@ void printAns(int pageSize,vector<vector<int>> ans,vector<int> list){
 
 }
 
-vector<int> Fifo(queue<int>& que,vector<int>& curr,vector<int> list, int pageSize, int pos){
+vector<int> Fifo(queue<int>& que,vector<int>& frame,vector<int> list, int pageSize, int pos){
     int fault = que.front();
     que.pop();
-    int index = findIndex(curr,pageSize,fault);
-    curr[index] = list[pos];
+    int index = findIndex(frame,pageSize,fault);
+    frame[index] = list[pos];
     que.push(list[pos]);
-    return curr;
+    return frame;
 }
 
 int main() {
     
     vector<int> list = {7,0,1,2,0,3,0,4,2,3,0,3,2,1,2,0,1,7,0,1};
     int pageSize = 3;
-    vector<int> curr;
+    vector<int> frame;
     vector<vector<int>> ans;
     queue<int> que;
     int n= pageSize;
        
     for(int i=0;i<list.size();i++){
         if(que.size()<pageSize){
-            curr.push_back(list[i]);
+            frame.push_back(list[i]);
             que.push(list[i]);
         }    
-        else if(findIndex(curr,pageSize,list[i])==-1){
-                curr = Fifo(que,curr,list,pageSize,i); 
+        else if(findIndex(frame,pageSize,list[i])==-1){
+                frame = Fifo(que,frame,list,pageSize,i); 
                 n++;    
         }
-        ans.push_back(curr);     
+        ans.push_back(frame);     
     }
     printAns(pageSize,ans,list);
     cout<<"N: "<<n<<endl;
