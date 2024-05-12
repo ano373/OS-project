a5171c227fa71ddd2df75e3f3c1525163c27003b
ano373 <96596021+ano373@users.noreply.github.com>
2024-05-12 22:37:02 +0300

    Update MFU.cpp

--- MFU.cpp
+++ MFU.cpp
@@ -53,9 +53,9 @@ int HighestFreqValue(unordered_map<int, int>& map){
 
 }
 
-unordered_map<int, int> trackFreq(vector<int>& list, int index, vector<int>& curr) {
+unordered_map<int, int> trackFreq(vector<int>& list, int index, vector<int>& frame) {
     unordered_map<int, int> map;
-    for (int val : curr) {
+    for (int val : frame) {
         int freq = 0;
         for (int i = 0; i <= index; ++i) {
             if (list[i] == val)
@@ -67,24 +67,24 @@ unordered_map<int, int> trackFreq(vector<int>& list, int index, vector<int>& cur
     return map;
 }
 
-vector<int> MRU(vector<int> curr,vector<int> list,int pos,int pageSize){
-    int index = findIndex(curr,pageSize,list[pos-1]);
-    curr[index] = list[pos];
-    return curr;
+vector<int> MRU(vector<int> frame,vector<int> list,int pos,int pageSize){
+    int index = findIndex(frame,pageSize,list[pos-1]);
+    frame[index] = list[pos];
+    return frame;
 
 }
 
-vector<int> MFU(vector<int>& curr,vector<int>& list,int pos, int pageSize){
-    unordered_map<int, int> map = trackFreq(list,pos-1,curr);
+vector<int> MFU(vector<int>& frame,vector<int>& list,int pos, int pageSize){
+    unordered_map<int, int> map = trackFreq(list,pos-1,frame);
     if(!AllSameFreq(map)){
         int value = HighestFreqValue(map);
-        int index = findIndex(curr,pageSize,value);
-        curr[index] =  list[pos];
+        int index = findIndex(frame,pageSize,value);
+        frame[index] =  list[pos];
     }
-    else curr = MRU(curr,list,pos,pageSize);
+    else frame = MRU(frame,list,pos,pageSize);
 
     
-    return curr;
+    return frame;
 }
 
 
@@ -92,19 +92,19 @@ vector<int> MFU(vector<int>& curr,vector<int>& list,int pos, int pageSize){
 int main(){
     vector<int> list = {7,0,1,2,0,3,0,4,2,3,0,3,2,1,2,0,1,7,0,1};
     int pageSize = 3;
-    vector<int> curr;
+    vector<int> frame;
     vector<vector<int>> ans;
     int n=pageSize;
 
     for(int i=0;i<list.size();i++){
         if(i<pageSize){
-            curr.push_back(list[i]);
+            frame.push_back(list[i]);
 
-        }else if(findIndex(curr,pageSize,list[i])==NOT_FOUND){
-            curr = MFU(curr,list,i,pageSize);
+        }else if(findIndex(frame,pageSize,list[i])==NOT_FOUND){
+            frame = MFU(frame,list,i,pageSize);
             n++;
             }
-        ans.push_back(curr);
+        ans.push_back(frame);
     }
     printAns(pageSize,ans,list);
     cout<<"N: "<<n<<endl;
